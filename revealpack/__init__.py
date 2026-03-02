baf53677ae482f0f4035893c7ace682d2ad7e1d4
Khris Griffis <khrisgriffis@ucla.edu>
2026-03-02 11:02:29 -0800

    version bump for cleanup fix

--- revealpack/__init__.py
+++ revealpack/__init__.py
@@ -2,7 +2,7 @@ import os
 import json
 import shutil
 
-__version__ = "1.4.8"
+__version__ = "1.4.9"
 __description__ = "RevealPack - A CLI tool for managing Reveal.js presentation packages"
 
 def copy_file_or_directory(src, dest):
